5ee3833852d64c1e1bee2bab8d978e86a424710b
Nurudee Adedeji <djsmacker01@gmail.com>
2025-10-17 12:44:13 +0100

    commit all

---
+++ basic_variables.py
@@ -0,0 +1,25 @@
+# Variabe
+
+# name = input("What is your name? ")
+# age = int(input("What is your age? "))
+
+# print(f"Hello, {name}! You look {age}")
+
+#List and loops
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+
+# tech_stack = ["Python", "JavaScript", "React", "Node.js", "MongoDB"]
+# for tech in tech_stack:
+#     print(tech)
+#Dictionary
+person = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+print(person["name"])
+
+#Functions
